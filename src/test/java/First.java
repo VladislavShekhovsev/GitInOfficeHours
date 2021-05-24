b6d20650c5137cdedd3081841f0753a8c61625fc
Vladislav <vladislav13457@gmail.com>
2021-05-24 18:28:55 -0400

    added some notes to first file

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -1,4 +1,6 @@
 public class First {
     // in gitignore just tap Ctrl+s to save the change like in Sublime
+    // to share project on Github.com just tap on Git/GitHub/share project on github
+    // (that only for the first time) after the first time u can just push
     // new line in second push
 }
